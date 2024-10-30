19aaf4a9a9417d5f7c4e8effc8f6fd97c63b805a
Seth Horsley <github.dvxhi@sl.isethi.com>
2024-10-30 19:44:38 +0100

    fix typography bug and enhance
    fix bug where integer would not convert to string and also added
    tailwind classes as option for size param

--- lib/rbui/typography/text.rb
+++ lib/rbui/typography/text.rb
@@ -27,20 +27,21 @@ module RBUI
 
     def size_to_class
       {
-        "1" => "text-xs",
-        "2" => "text-sm",
-        "3" => "text-base",
-        "4" => "text-lg",
-        "5" => "text-xl",
-        "6" => "text-2xl",
-        "7" => "text-3xl",
-        "8" => "text-4xl",
-        "9" => "text-5xl"
+        "1" => "text-xs", "xs" => "text-xs",
+        "2" => "text-sm", "sm" => "text-sm",
+        "3" => "text-base", "base" => "text-base",
+        "4" => "text-lg", "lg" => "text-lg",
+        "5" => "text-xl", "xl" => "text-xl",
+        "6" => "text-2xl", "2xl" => "text-2xl",
+        "7" => "text-3xl", "3xl" => "text-3xl",
+        "8" => "text-4xl", "4xl" => "text-4xl",
+        "9" => "text-5xl", "5xl" => "text-5xl"
       }
     end
 
     def weight_to_class
       {
+        "muted" => "text-muted-foreground",
         "light" => "font-light",
         "regular" => "font-normal",
         "medium" => "font-medium",
