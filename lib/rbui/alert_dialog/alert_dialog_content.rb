3aecd0adb1b8636b7cc6f38e9bbb1fbe501b5839
Seth Horsley <github.dvxhi@sl.isethi.com>
2024-11-04 20:17:09 +0100

    clean up

--- lib/rbui/alert_dialog/alert_dialog_content.rb
+++ lib/rbui/alert_dialog/alert_dialog_content.rb
@@ -3,7 +3,7 @@
 module RBUI
   class AlertDialogContent < Base
     def view_template(&block)
-      div(**attrs.merge(class: "hidden")) do
+      div(class: "hidden") do
         div(data: {controller: "rbui--alert-dialog"}) do
           background
           container(&block)
